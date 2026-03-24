2ee65ea41d37ebb0507ce6f88558769c219696b1
grabieltec-front-end <supremacia692@gmail.com>
2026-03-23 21:38:12 -0300

    Feat:correction index.ts Home export

--- src/page/index.ts
+++ src/page/index.ts
@@ -1,4 +1,5 @@
 export { default as Cadastro } from './Registration'
+export { default as Home } from './Home'
 export { default as Leads } from './Leads'
 export { default as Login } from './Login'
 export { default as Perfil } from './Perfil'
