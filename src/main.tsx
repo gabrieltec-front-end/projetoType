8d5bea4d205b9693b83594e83c95043295be28cd
grabieltec-front-end <supremacia692@gmail.com>
2026-04-20 21:55:17 -0300

    agora foi

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { ThemeProvider } from 'styled-components'
 import { createRoot } from 'react-dom/client'
 
 import App from './App.tsx'
-import { GlobalStyle, darkTheme, lighThemes } from './styles/'
+import { GlobalStyle, lighThemes } from './styles/'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
