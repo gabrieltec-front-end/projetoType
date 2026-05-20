11606a50a45b19d01f31194b75f5dea6736aa022
grabieltec-front-end <supremacia692@gmail.com>
2026-05-20 20:21:51 -0300

    Concluid,Context feat/create-appContext

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
 import { StrictMode } from 'react'
-import { ThemeProvider } from 'styled-components'
 import { createRoot } from 'react-dom/client'
 
 import App from './App.tsx'
-import { GlobalStyle, lighThemes } from './styles/'
+import { GlobalStyle } from './styles/'
+import { AppThemeProvider } from './contexts/ThemeContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider theme={lighThemes}>
+    <AppThemeProvider>
       <GlobalStyle />
       <App />
-    </ThemeProvider>
+    </AppThemeProvider>
   </StrictMode>
 )
